5e1ea455e68a4bee0ae8b5fa60be44c1f9378e3e
Alexander Iovtcehv <x0063413@ti.com>
2015-07-23 20:52:02 +0300

    Refactored IHEngine's keyboard handler

--- gameSample/Engine/InputHandlerEngine.js
+++ gameSample/Engine/InputHandlerEngine.js
@@ -1,49 +1,30 @@
 var InputHandlerEngine = (function InputHandlerEngine(){
-	var inputHandlerEngine = {};
-	
-	inputHandlerEngine.init = function() 
-	{
-		inputHandlerEngine.moves = [];
-		
-		return this;
-	};
-	
-	inputHandlerEngine.handleKeyboardInput = function()
-	{
-		document.onkeydown = function(e)
+	var inputHandlerEngine = {
+		init: function() 
 		{
-			inputHandlerEngine.moves = [];
+			return this;
+		},
+		handleKeyboardInput: function()
+		{
+			var moves = [];
+			window.addEventListener('keydown', function(e){
+				if(e.keyCode === 37) {
+					moves.push('left');
+				} else if (e.keyCode === 38) {
+					moves.push('up');
+				} else if (e.keyCode === 39) {
+					moves.push('right');
+				} else if (e.keyCode === 40) {
+					moves.push('down');
+				}
+			});
 			
-			switch (e.keyCode)
-			{
-				case (38 || 87): //arrow up or w
-					console.log('up');
-					inputHandlerEngine.moves.push('up');
-					break;
-				case (37 || 65): //arrow left or a
-					console.log('left');
-					inputHandlerEngine.moves.push('left');
-					break;
-				case (39 || 68): //arrow right or d
-					console.log('right');
-					inputHandlerEngine.moves.push('right');
-					break;
-				case (40 || 83): //arrow down or s
-					console.log('down');
-					inputHandlerEngine.moves.push('down');
-					break;
-				default:
-					break;
-			}
-		} 
-		
-		return inputHandlerEngine.moves;
+			return moves;
+		},
+		handleMouseInput: function()
+		{
+			//TODO: handle menu screen input
+		}
 	};
-	
-	inputHandlerEngine.handleMouseInput = function()
-	{
-		//TODO: handle menu screen input
-	};
-	
 	return inputHandlerEngine;
 }());
