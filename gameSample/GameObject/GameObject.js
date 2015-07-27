fb78442495286408b67879bf07af8713db13b6ce
Teodor <tedko_97@abv.bg>
2015-07-27 19:22:23 +0300

    Added collision check for GameObject.js

--- gameSample/GameObject/GameObject.js
+++ gameSample/GameObject/GameObject.js
@@ -15,6 +15,34 @@ var GameObject = (function()
 
         return this;
     };
+	
+	GameObject.collideWith = function(anotherGameObj)
+    {
+        var gameObjLeft= this.x,
+            gameObjRight = gameObjLeft+ this.w,
+            gameObjTop = this.y,
+            gameObjBottom = gameObjTop + this.h;
+
+        var anotherGameObjLeft = anotherGameObj.x,
+            anotherGameObjRight = anotherGameObjLeft + anotherGameObj.w,
+            anotherGameObjTop = anotherGameObj.y,
+            anotherGameObjBottom = anotherGameObjTop + anotherGameObj.h;
+
+        var anotherObjRightWithinGameObj = (gameObjLeft < anotherGameObjRight && anotherGameObjRight < gameObjRight),
+            anotherObjLeftWithinGameObj = (gameObjLeft < anotherGameObjLeft && anotherGameObjLeft < gameObjRight),
+            anotherObjTopWithinGameObj = (anotherGameObjBottom < gameObjTop && gameObjTop < anotherGameObjTop),
+            anotherObjBottomWithinGameObj = (anotherGameObjBottom < gameObjBottom && gameObjBottom < anotherGameObjTop);
+
+        // Player left or right side is between both left and right sides of the npc
+        if(anotherObjRightWithinGameObj || anotherObjLeftWithinGameObj)
+        {
+            // NPC top or bottom side is between both the top and down player sides
+            if(anotherObjBottomWithinGameObj || anotherObjTopWithinGameObj)
+            {
+                alert("Game object collided with another game object");
+            }
+        }
+    };
 
     GameObject.render = function(ctx)
     {
